afb26c06bc058c76c38801758104a403a34f4735
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-18 08:06:40 +0000

    Add OMW endpoint to get data

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import os
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
 OMW_KEY = os.environ.get("OMW_API_KEY")
+OMW_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?"
+
+parameters = {
+    "lat": os.environ.get("LAT"),
+    "lon": os.environ.get("LONG"),
+    "appid": OMW_KEY
+}
+
+response = requests.get(OMW_ENDPOINT, params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
