b62e22839681c39fd31e81ce965f217d8eff0071
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-18 09:01:22 +0000

    Add twilio sms api

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
 import requests
+from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Open configs
 OMW_KEY = os.environ.get("OMW_API_KEY")
 OMW_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?"
+
+# Twilio configs
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 parameters = {
     "lat": os.environ.get("LAT"),
@@ -35,4 +42,8 @@ for hr_condition in half_day_weather:
 
 # Provide a feedback if there's rain
 if will_rain:
-    print("Take an umbrella on your way out.")
+    message = client.messages.create(
+        body="Take an umbrella 🌂 on your way out, It is going to rain 🌧 today. From: A3AJAGBE RAIN ALERT",
+        from_=os.environ.get('TWILIO_NUMBER'),
+        to=os.environ.get('MY_NUMBER')
+    )
